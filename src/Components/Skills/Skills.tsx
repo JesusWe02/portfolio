c45251b341b188fc91d1019c3dcd80be4a7159fd
JesusWe <jesuswe02@gmail.com>
2023-12-30 01:39:57 -0500

    skills test

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,4 +1,78 @@
 import "./Skills.css";
+import htmlImg from "../../icons/html.svg";
+import cssImg from "../../icons/css.svg";
+import jsImg from "../../icons/js.svg";
+import typeScriptImg from "../../icons/typescript.svg";
+import reactImg from "../../icons/react.svg";
+import gitImg from "../../icons/git-black.svg";
+
+import javaImg from "../../icons/java-black.svg";
+import mysqlImg from "../../icons/mysql-black.svg";
+import githublImg from "../../icons/github-logo.svg";
+import bootstrapImg from "../../icons/bootstrap.svg";
+
+import moduloI from "../../imgs/CertificadoI.png";
+import moduloII from "../../imgs/CertificadoII.png";
+import ceprotec from "../../imgs/CeprotecCertificado.png";
+import jobs from "../../imgs/JobsCetificado.png";
+
 export function Skills() {
-  return <section className="container-skills">fdsfsf</section>;
+  return (
+    <section className="main-section">
+      <article className="aboutMe-section">
+        <header>
+          <h2>Sobre mi</h2>
+        </header>
+        <section className="container-aboutMe">
+          <div className="firts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              maxime quo inventore aperiam corrupti animi saepe assumenda
+              dolorem veritatis consequatur, corporis ratione nemo, quae impedit
+              magnam quas sit laudantium blanditiis. Lorem ipsum dolor sit amet
+            </p>
+          </div>
+          <div className="second">
+            <img src={moduloI} alt="certificadoTest" />
+          </div>
+          <div className="third">
+            <img src={moduloII} alt="certificadoTest" />
+          </div>
+          <div className="fourth">
+            <img src={jobs} alt="certificadoTest" />
+          </div>
+          <div className="fifth">
+            <img src={moduloII} alt="certificadoTest" />
+          </div>
+          <div className="six">
+            <img src={ceprotec} alt="certificadoTest" />
+          </div>
+        </section>
+      </article>
+      <article className="skills-section">
+        <header>
+          <h2>Skills</h2>
+        </header>
+        <section className="skills">
+          <div>
+            <img src={htmlImg} alt="htmlIcon" />
+            <img src={cssImg} alt="cssIcon" />
+            <img src={jsImg} alt="jsIcon" />
+            <img src={typeScriptImg} alt="typeScriptIcon" />
+            <img src={reactImg} alt="reactIcon" />
+            <img src={gitImg} alt="gitIcon" />
+          </div>
+        </section>
+        <section className="other-skills">
+          <h3>Otros</h3>
+          <div>
+            <img src={bootstrapImg} alt="bootstrapIcon" />
+            <img src={githublImg} alt="githubIcon" />
+            <img src={mysqlImg} alt="mysqlIcon" />
+            <img src={javaImg} alt="javaIcon" />
+          </div>
+        </section>
+      </article>
+    </section>
+  );
 }
