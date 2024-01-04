e5d758b8b05280f45d3b28142b9e9c6bfc0c59f1
JesusWe <jesuswe02@gmail.com>
2024-01-03 20:20:58 -0500

    footer implement

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,6 +1,15 @@
 import { useRef } from "react";
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDiscord,
+  faGithub,
+  faLinkedin,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 export function Contact() {
   const form = useRef<HTMLFormElement | null>(null);
@@ -39,6 +48,49 @@ export function Contact() {
         <textarea name="message" placeholder="Message" required />
         <input type="submit" value="Send" />
       </form>
+      <footer className="container-footer">
+        <ul className="social-icons">
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faDiscord} />
+              {/* <FontAwesomeIcon icon={faSquareInstagram} /> */}
+            </a>
+          </li>
+        </ul>
+        <div className="contact-info">
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faPhone} />
+              <span>+51 967632230</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+              <span>jesuswe02@gmail.com</span>
+            </li>
+          </ul>
+        </div>
+        <p>Copyright © 2024 | All Rights Reserved</p>
+      </footer>
     </section>
   );
 }
