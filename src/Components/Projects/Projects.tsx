4a82d62bdc795a4eed9ede016388d9e97b5aa754
JesusWe02 <jesuswe02@gmail.com>
2024-02-08 17:17:03 -0500

    add styles projects section

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,46 +1,47 @@
-import "./Projects.css";
-import proyect1Img from "../../imgs/proyectoIMG.jpg";
-import proyect2Img from "../../imgs/proyectoIMG.jpg";
-import proyect3Img from "../../imgs/proyectoIMG.jpg";
-import proyect4Img from "../../imgs/proyectoIMG.jpg";
-import proyect5Img from "../../imgs/proyectoIMG.jpg";
-import proyect6Img from "../../imgs/proyectoIMG.jpg";
+import './Projects.css';
+import proyect1Img from '../../imgs/proyectoIMG.jpg';
+import proyect2Img from '../../imgs/proyectoIMG.jpg';
+import proyect3Img from '../../imgs/proyectoIMG.jpg';
+import proyect4Img from '../../imgs/proyectoIMG.jpg';
+import proyect5Img from '../../imgs/proyectoIMG.jpg';
+import proyect6Img from '../../imgs/proyectoIMG.jpg';
 
 const porjectsData = [
   {
     id: 1,
-    title: "Proyecto 1",
-    description: "Descripción del proyecto 1",
+    title: 'Yu-Gi-Oh Life Counter',
+    description: 'Website created for a client.',
     image: proyect1Img,
   },
   {
     id: 2,
-    title: "Proyecto 2",
-    description: "Descripción del proyecto 2",
+    title: 'E-Comerce for my work',
+    description: 'Still in progress to deploy it into production.',
     image: proyect2Img,
   },
   {
     id: 3,
-    title: "Proyecto 3",
-    description: "Descripción del proyecto 3",
+    title: 'E-Comerce for my workzzzssss',
+    description:
+      'Still in progress to deploy it into productions adasd ss jhbjhy.',
     image: proyect3Img,
   },
   {
     id: 4,
-    title: "Proyecto 4",
-    description: "Descripción del proyecto 4",
+    title: 'Project 4',
+    description: 'Project Description 4',
     image: proyect4Img,
   },
   {
     id: 5,
-    title: "Proyecto 5",
-    description: "Descripción del proyecto 5",
+    title: 'Project 5',
+    description: 'Project Description 5',
     image: proyect5Img,
   },
   {
     id: 6,
-    title: "Proyecto 6",
-    description: "Descripción del proyecto 6",
+    title: 'Project 6',
+    description: 'Project Description 6',
     image: proyect6Img,
   },
 ];
@@ -54,6 +55,7 @@ export function Projects() {
         {porjectsData.map((project) => (
           <article key={project.id}>
             <img src={project.image} alt={project.title} />
+            <span className="split"></span>
             <h3>{project.title}</h3>
             <p>{project.description}</p>
           </article>
