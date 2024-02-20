8aadab261e74d95a4f405cd91e31760cf075dad4
JesusWe <jesuswe02@gmail.com>
2024-02-20 00:47:52 -0500

    test modal implement

--- src/Assets/assets.ts
+++ src/Assets/assets.ts
@@ -1,48 +1,48 @@
-import htmlIcon from '../icons/html.svg';
-import cssIcon from '../icons/css.svg';
-import jsIcon from '../icons/js.svg';
-import typeScriptIcon from '../icons/typescript.svg';
-import reactIcon from '../icons/react.svg';
-import gitIcon from '../icons/git-black.svg';
+import htmlIcon from "../icons/html.svg";
+import cssIcon from "../icons/css.svg";
+import jsIcon from "../icons/js.svg";
+import typeScriptIcon from "../icons/typescript.svg";
+import reactIcon from "../icons/react.svg";
+import gitIcon from "../icons/git-black.svg";
 
-import javaIcon from '../icons/java-black.svg';
-import mysqlIcon from '../icons/mysql-black.svg';
-import githubIcon from '../icons/github-logo.svg';
-import bootstrapIcon from '../icons/bootstrap.svg';
+import javaIcon from "../icons/java-black.svg";
+import mysqlIcon from "../icons/mysql-black.svg";
+import githubIcon from "../icons/github-logo.svg";
+import bootstrapIcon from "../icons/bootstrap.svg";
 
-import testTitulo from '../imgs/tituloTest.png';
-import moduloI from '../imgs/CertificadoI.png';
-import moduloII from '../imgs/CertificadoII.png';
-import ceprotec from '../imgs/CeprotecCertificado.png';
-import jobs from '../imgs/JobsCetificado.png';
+import testTitulo from "../imgs/tituloTest.png";
+import moduloI from "../imgs/CertificadoI.png";
+import moduloII from "../imgs/CertificadoII.png";
+import ceprotec from "../imgs/CeprotecCertificado.png";
+import jobs from "../imgs/JobsCetificado.png";
 
-import expandModuloI from '../imgs/ExpandModuloI.png';
-import expandModuloII from '../imgs/ExpandModuloII.png';
-import expandJobs from '../imgs/ExpandJobs.png';
-import expandCeprotec from '../imgs/ExpandCeprotec.png';
+import expandModuloI from "../imgs/ExpandModuloI.png";
+import expandModuloII from "../imgs/ExpandModuloII.png";
+import expandJobs from "../imgs/ExpandJobs.png";
+import expandCeprotec from "../imgs/ExpandCeprotec.png";
 
 const certificates = [
-  { imgSrc: testTitulo, alt: 'Titulo Cibertec', expandSrc: testTitulo },
-  { imgSrc: moduloI, alt: 'Modulo I', expandSrc: expandModuloI },
-  { imgSrc: moduloII, alt: 'Modulo II', expandSrc: expandModuloII },
-  { imgSrc: ceprotec, alt: 'Ceprotec', expandSrc: expandCeprotec },
-  { imgSrc: jobs, alt: 'Jobs', expandSrc: expandJobs },
+  { imgSrc: testTitulo, alt: "Titulo Cibertec", expandSrc: testTitulo },
+  { imgSrc: moduloI, alt: "Modulo I", expandSrc: expandModuloI },
+  { imgSrc: moduloII, alt: "Modulo II", expandSrc: expandModuloII },
+  { imgSrc: jobs, alt: "Jobs", expandSrc: expandJobs },
+  { imgSrc: ceprotec, alt: "Ceprotec", expandSrc: expandCeprotec },
 ];
 
 const skillsIcons = [
-  { src: htmlIcon, alt: 'htmlIcon' },
-  { src: cssIcon, alt: 'cssIcon' },
-  { src: jsIcon, alt: 'jsIcon' },
-  { src: typeScriptIcon, alt: 'typeScriptIcon' },
-  { src: reactIcon, alt: 'reactIcon' },
-  { src: gitIcon, alt: 'gitIcon' },
+  { src: htmlIcon, alt: "htmlIcon" },
+  { src: cssIcon, alt: "cssIcon" },
+  { src: jsIcon, alt: "jsIcon" },
+  { src: typeScriptIcon, alt: "typeScriptIcon" },
+  { src: reactIcon, alt: "reactIcon" },
+  { src: gitIcon, alt: "gitIcon" },
 ];
 
 const otherSkillsIcons = [
-  { src: bootstrapIcon, alt: 'bootstrapIcon' },
-  { src: githubIcon, alt: 'githubIcon' },
-  { src: mysqlIcon, alt: 'mysqlIcon' },
-  { src: javaIcon, alt: 'javaIcon' },
+  { src: bootstrapIcon, alt: "bootstrapIcon" },
+  { src: githubIcon, alt: "githubIcon" },
+  { src: mysqlIcon, alt: "mysqlIcon" },
+  { src: javaIcon, alt: "javaIcon" },
 ];
 
 export { certificates, skillsIcons, otherSkillsIcons };
--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,157 +1,48 @@
-{
-  /*
-
-  import Modal from '../Modal/Modal';
-  import './Skills.css';
-  
-  import htmlImg from '../../icons/html.svg';
-  import cssImg from '../../icons/css.svg';
-  import jsImg from '../../icons/js.svg';
-  import typeScriptImg from '../../icons/typescript.svg';
-  import reactImg from '../../icons/react.svg';
-  import gitImg from '../../icons/git-black.svg';
-  
-  import javaImg from '../../icons/java-black.svg';
-  import mysqlImg from '../../icons/mysql-black.svg';
-  import githublImg from '../../icons/github-logo.svg';
-  import bootstrapImg from '../../icons/bootstrap.svg';
-  
-  import testTitulo from '../../imgs/testCertificado.jpg';
-  import moduloI from '../../imgs/CertificadoI.png';
-  import moduloII from '../../imgs/CertificadoII.png';
-  import ceprotec from '../../imgs/CeprotecCertificado.png';
-  import jobs from '../../imgs/JobsCetificado.png';
-  
-  import expandModuloI from '../../imgs/ExpandModuloI.png';
-  import expandModuloII from '../../imgs/ExpandModuloII.png';
-  import expandJobs from '../../imgs/ExpandJobs.png';
-  import expandCeprotec from '../../imgs/ExpandCeprotec.png';
-  
-  export function Skills() {
-    return (
-      <section className="section-skills" id="skills">
-        <article className="aboutMe-section">
-          <header>
-            <h2>About Me</h2>
-          </header>
-          <section className="container-aboutMe">
-            <div className="first">
-              <div className="first-content">
-                <p>
-                  Hello, I'm Jesus! I'm new to the world of programming and strive
-                  to get better every day. Since childhood, I've always been
-                  passionate about everything related to technology and computers,
-                  leading me to assemble my first computer at a young age. In my
-                  free time, I enjoy assembling and maintaining computers, as well
-                  as solving issues for my clients.
-                </p>
-                <br />
-                <p>
-                  Currently, I am looking to enter this new field of programming
-                  to grow professionally and, above all, as an individual.
-                </p>
-              </div>
-            </div>
-            <div className="second">
-              <Modal
-                trigger={<img src={testTitulo} alt="Titulo Cibertec" />}
-                maxWidth="1100px"
-                maxHeight="90%"
-              >
-                <img
-                  src={testTitulo}
-                  alt="Titulo Cibertec"
-                  className="imgModal"
-                />
-              </Modal>
-            </div>
-            <div className="third">
-              <Modal
-                trigger={<img src={moduloI} alt="Modulo I" />}
-                maxWidth="1100px"
-                maxHeight="90%"
-              >
-                <img src={expandModuloI} alt="ModuloI" className="imgModal" />
-              </Modal>
-            </div>
-            <div className="fourth">
-              <Modal
-                trigger={<img src={jobs} alt="certificadoTest" />}
-                maxWidth="540px"
-                maxHeight="90%"
-              >
-                <img
-                  src={expandJobs}
-                  alt="CertificadoJobs"
-                  className="imgModal"
-                />
-              </Modal>
-            </div>
-            <div className="fifth">
-              <Modal
-                trigger={<img src={moduloII} alt="certificadoTest" />}
-                maxWidth="1100px"
-                maxHeight="90%"
-              >
-                <img src={expandModuloII} alt="ModuloI" className="imgModal" />
-              </Modal>
-            </div>
-            <div className="six">
-              <Modal
-                trigger={<img src={ceprotec} alt="certificadoTest" />}
-                maxWidth="1100px"
-                maxHeight="90%"
-              >
-                <img src={expandCeprotec} alt="ModuloI" className="imgModal" />
-              </Modal>
-            </div>
-          </section>
-        </article>
-        <article className="skills-section">
-          <header>
-            <h2>Skills</h2>
-          </header>
-          <section className="skills">
-            <div>
-              <img src={htmlImg} alt="htmlIcon" />
-              <img src={cssImg} alt="cssIcon" />
-              <img src={jsImg} alt="jsIcon" />
-              <img src={typeScriptImg} alt="typeScriptIcon" />
-              <img src={reactImg} alt="reactIcon" />
-              <img src={gitImg} alt="gitIcon" />
-            </div>
-          </section>
-          <section className="other-skills">
-            <h3>Others</h3>
-            <div>
-              <img src={bootstrapImg} alt="bootstrapIcon" />
-              <img src={githublImg} alt="githubIcon" />
-              <img src={mysqlImg} alt="mysqlIcon" />
-              <img src={javaImg} alt="javaIcon" />
-            </div>
-          </section>
-        </article>
-      
-      </section>
-    );
-  }
-
-
-
-
-
- */
-}
-
-import Modal from '../Modal/Modal';
-import './Skills.css';
+import "./Skills.css";
 import {
   certificates,
   skillsIcons,
   otherSkillsIcons,
-} from '../../Assets/assets';
+} from "../../Assets/assets";
+import { useEffect, useState } from "react";
 
 export function Skills() {
+  const [selectedCertificateIndex, setSelectedCertificateIndex] = useState<
+    number | null
+  >(null);
+  const [modalActive, setModalActive] = useState(false);
+
+  const toggleModal = (certIndex: number) => {
+    setSelectedCertificateIndex(
+      certIndex === selectedCertificateIndex ? null : certIndex
+    );
+    setModalActive(!modalActive);
+  };
+  {
+    /*
+
+  useEffect(() => {
+    if (modalActive) {
+      document.body.classList.add("active-modal");
+    } else {
+      document.body.classList.remove("active-modal");
+    }
+    return () => {
+      document.body.classList.remove("active-modal");
+    };
+  }, [modalActive]);
+
+  */
+  }
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("active-modal", modalActive);
+
+    return () => {
+      document.documentElement.classList.remove("active-modal");
+    };
+  }, [modalActive]);
+
   return (
     <section className="section-skills" id="skills">
       <article className="aboutMe-section">
@@ -177,14 +68,12 @@ export function Skills() {
             </div>
           </div>
           {certificates.map((cert, index) => (
-            <div className={`cert-section cert-${index}`} key={index}>
-              <Modal
-                trigger={<img src={cert.imgSrc} alt={cert.alt} />}
-                maxWidth="1100px"
-                maxHeight="90%"
-              >
-                <img src={cert.expandSrc} alt={cert.alt} className="imgModal" />
-              </Modal>
+            <div
+              className={`cert-section cert-${index}`}
+              key={index}
+              onClick={() => toggleModal(index)}
+            >
+              <img src={cert.imgSrc} alt={cert.alt} />
             </div>
           ))}
         </section>
@@ -209,6 +98,31 @@ export function Skills() {
           </div>
         </section>
       </article>
+
+      {/* Modal */}
+      {selectedCertificateIndex !== null && (
+        <div className="modal">
+          <div
+            className="overlay"
+            onClick={() => toggleModal(selectedCertificateIndex)}
+          ></div>
+          <div className="modal-content">
+            {certificates[selectedCertificateIndex].expandSrc && (
+              <img
+                src={certificates[selectedCertificateIndex].expandSrc}
+                alt={certificates[selectedCertificateIndex].alt}
+                className="imgModal"
+              />
+            )}
+            <button
+              className="close-modal"
+              onClick={() => toggleModal(selectedCertificateIndex)}
+            >
+              &times;
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
